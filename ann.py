48e314595c4ba9435262a6025a297d191a4550b7
Erik <erik@nova.local>
2019-09-06 11:13:53 -0500

    added library and subsequent controller code

---
+++ ann.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+"""
+Simple small neural network library.
+Elegantly written in Python with Numpy.
+
+Erik Quintanilla
+
+"""
+
+
+def sigmoid(x):
+        return np.exp(x)/(np.exp(x) +1 )
+
+def relu(x):
+        return np.maximum(x, 0)
+
+def softmax(x):
+        return np.exp(x)/sum(np.exp(x))
+
+def cross_entropy(y, y_hat):
+        return np.dot(-y,np.log(y_hat))
+
+
+
+class NN():
+    def __init__(self, layers = []):
+        self.layers = layers
+
+    def forward(self, x):
+        for i in range(len(self.layers)):
+            if i==0:
+                self.layers[i].forward(x)
+            else:
+                previous = self.layers[i-1].output
+                self.layers[i].forward(previous)
+
+            if i == len(self.layers)-1:
+                print("Output Vector: ", self.layers[i].output)
+                # print("Weights: ", self.layers[i].weights)
+
+            # print(self.layers[i].weights)
+            # print(self.layers[i].bias)
+            # print(self.layers[i].output)
+
+    def backward(self, inputs, ground_truth):
+        for i in range(len(self.layers)-1, -1, -1):
+            if i == len(self.layers)-1:
+                # we should assume the output layer isn't going to be relu...
+                y = ground_truth
+                y_hat = self.layers[i].output
+                print("Loss: %f" %(cross_entropy(y, y_hat)))
+                dLdb = y_hat - y
+                dLdW = np.outer(dLdb, self.layers[i-1].output)
+                self.layers[i].bias -= dLdb
+                self.layers[i].weights -= dLdW
+                self.layers[i].dLdb = dLdb
+                self.layers[i].dLdW = dLdW
+            else:
+                Z = self.layers[i].output
+                W_next = self.layers[i+1].weights
+                dLdb_next = self.layers[i+1].dLdb
+                Z_prev = self.layers[i-1].output
+
+                if self.layers[i].activation == "relu":
+                     dLdb = (W_next.T@(dLdb_next))*(np.heaviside(Z, 0))
+                else:
+                    dLdb = (W_next.T@(dLdb_next))*Z*(1-Z)
+
+                if i == 0:
+                    dLdW = np.outer(dLdb, inputs)
+                else:
+                    dLdW = np.outer(dLdb, Z_prev)
+                self.layers[i].bias -= dLdb
+                self.layers[i].weights -= dLdW
+                self.layers[i].dLdb = dLdb
+                self.layers[i].dLdW = dLdW
+
+    def print_weights(self):
+        for layer in self.layers:
+            print(layer.weights)
+
+
+class FCLayer():
+    def __init__(self, num_nodes, weights, activation = "sigmoid"):
+        self.weights = weights
+        self.bias = None
+        #self.bias = self.bias / (np.linalg.norm(self.bias)) #this should be uniform random noise
+        self.activation = activation
+        self.output = None
+        self.num_nodes = num_nodes
+        self.dLdb = None
+        self.dLdW = None
+
+    def forward(self, x):
+        wx = np.matmul(self.weights, x)
+        self.bias = np.zeros(wx.shape)
+        wx = np.add(wx, self.bias)
+
+        if self.activation == "sigmoid":
+                wx = sigmoid(wx)
+
+        elif self.activation == "relu":
+                wx = relu(wx)
+
+        elif self.activation == "softmax":
+                wx = softmax(wx)
+
+        self.output = wx
+
---
+++ controller.py
@@ -0,0 +1,46 @@
+from ann import *
+
+
+net = NN()
+
+# bias1 = np.ones((2,1))
+# this bias is automatically set to the unit vector within
+# the code.
+weights1 = [[0.1, 0.3, 0.7],[0.9, 0.4, 0.4]]
+weights1 = np.asarray(weights1)
+layer1 = FCLayer(num_nodes=3, weights = weights1, activation = "relu")
+net.layers.append(layer1)
+
+
+#bias2 = np.ones((2,1))
+weights2 = [[0.4, 0.3],[0.7, 0.2]]
+weights2 = np.asarray(weights2)
+layer2 = FCLayer(num_nodes=2, weights = weights2, activation = "sigmoid")
+net.layers.append(layer2)
+
+
+#bias3 = np.ones((2,1))
+weights3 = [[0.5, 0.6],[0.6, 0.7], [0.3, 0.2]]
+weights3 = np.asarray(weights3)
+layer3 = FCLayer(num_nodes=2, weights = weights3, activation = "softmax")
+net.layers.append(layer3)
+
+
+inputs = np.array([0.1, 0.7, 0.3])
+ground_truth = np.array([1.,0.,0.])
+n_epochs = 200
+for x in range(1,n_epochs+1):
+    print("EPOCH: %i/%i" %(x, n_epochs))
+    net.forward(inputs)
+    net.backward(inputs, ground_truth)
+
+net.print_weights()
+
+
+
+
+
+
+
+
+
