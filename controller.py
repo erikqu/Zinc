006f6ffd1ca6bc7121e4764f1b7be92e53065616
Erik <erik@nova.local>
2019-09-12 10:14:54 -0500

    correctly approximates xor

--- controller.py
+++ controller.py
@@ -5,14 +5,10 @@ input_shape = (2,)
 
 net = NN(input_shape = input_shape)
 
-layer1 = FCLayer(num_nodes=input_shape[0],  activation = "sigmoid")
-net.add_layer(layer1)
+net.add_layer(FCLayer(num_nodes=input_shape[0],  activation = "sigmoid"))
+net.add_layer(FCLayer(num_nodes=2, activation = "sigmoid"))
+net.add_layer(FCLayer(num_nodes=1, activation = "sigmoid"))
 
-layer2 = FCLayer(num_nodes=50, activation = "sigmoid")
-net.add_layer(layer2)
-
-layer3 = FCLayer(num_nodes=1, activation = "sigmoid")
-net.add_layer(layer3)
 
 net.compile()
 
@@ -28,14 +24,16 @@ def f(x):
         return 1
 
 
-xs = [np.random.randint(2, size=2) for x in range(0,200)]
+
+datalength = 1000
+n_epochs = 20
+
+xs = [np.random.randint(2, size=2) for x in range(0,datalength)]
 ys = [[f(xs[x])] for x in range(len(xs))]
 
 xs = np.asarray(xs)
 ys = np.asarray(ys)
 
-
-n_epochs = 1
 for x in range(1,n_epochs+1):
     for i in range(len(xs)):
         # print("INPUT", xs[i], xs[i].shape)
@@ -43,11 +41,14 @@ for x in range(1,n_epochs+1):
         net.backward(ys[i])
     print("EPOCH: %i/%i" %(x, n_epochs))
 
-while True:
-    inp = np.array([])
-    for x in range(input_shape[0]):
-        tmp = input("Enter element # %i >>>>" %(x))
-        inp = np.append(inp, float(tmp))
-    net.forward(inp)
-    response = net.layers[-1].output
-    print(response)
+manned_input = input("TRAINING COMPLETE\n\nWOULD YOU LIKE TO ENTER INPUT MANUALLY? (y/n)>")
+
+if manned_input.lower() == "y":
+    while True:
+        inp = np.array([])
+        for x in range(input_shape[0]):
+            tmp = input("Enter element # %i >>>>" %(x))
+            inp = np.append(inp, float(tmp))
+        net.forward(inp)
+        response = net.layers[-1].output
+        print(response)
